96dd21e433db67c4344a97920c886405240d3194
Alexander <WebDer11@gmail.com>
2023-05-30 15:47:34 +0400

    Finally 2

--- src/main/java/org/HW/Main.java
+++
@@ -1,7 +0,0 @@
-package org.HW;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/test/java/demoqa/tests/PracticeformTest.java
+++ src/test/java/demoqa/tests/PracticeformTest.java
@@ -1,16 +1,13 @@
 package demoqa.tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeformTest {
 
@@ -19,12 +16,14 @@ public class PracticeformTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
-
     @Test
-    void UiTest() {
-        open("https://demoqa.com/automation-practice-form");
+    void uiTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Egorov");
@@ -49,7 +48,7 @@ public class PracticeformTest {
         $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
 
-        $("#uploadPicture").uploadFile(new File("src/test/resources/new.png"));
+        $("#uploadPicture").uploadFromClasspath("new.png");
 
         $("#currentAddress").setValue("Moscow");
 
@@ -60,18 +59,19 @@ public class PracticeformTest {
         $("#submit").click();
 
 
-        $(byText("Thanks for submitting the form")).shouldBe(Condition.visible);
-        $(byText("Alex Egorov")).shouldBe(Condition.visible);
-        $(byText("Alex@egorov.com")).shouldBe(Condition.visible);
-        $(byText("Male")).shouldBe(Condition.visible);
-        $(byText("8888888888")).shouldBe(Condition.visible);
-        $(byText("22 September,1995")).shouldBe(Condition.visible);
-        $(byText("English, History")).shouldBe(Condition.visible);
-        $(byText("Sports, Reading, Music")).shouldBe(Condition.visible);
-        $(byText("new.png")).shouldBe(Condition.visible);
-        $(byText("Moscow")).shouldBe(Condition.visible);
-        $(byText("NCR Delhi")).shouldBe(Condition.visible);
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex Egorov"));
+        $(".table-responsive").shouldHave(text("Alex@egorov.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("8888888888"));
+        $(".table-responsive").shouldHave(text("22 September,1995"));
+        $(".table-responsive").shouldHave(text("English, History"));
+        $(".table-responsive").shouldHave(text("Sports, Reading, Music"));
+        $(".table-responsive").shouldHave(text("new.png"));
+        $(".table-responsive").shouldHave(text("Moscow"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
 
 
     }
+
 }
