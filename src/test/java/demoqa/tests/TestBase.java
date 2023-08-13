12a5ea8c4da75e1b467e1342c4abd0310e161548
Alexander <WebDer11@gmail.com>
2023-08-13 21:14:45 +0400

    HW RandomData

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -2,10 +2,19 @@ package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import demoqa.helpers.Attach;
 import demoqa.pages.RegistrationPage;
 import demoqa.utils.DataRandoms;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -17,8 +26,32 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.savePageSource = false;
         Configuration.screenshots = false;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("enableVCN", true);
+            put("enableVideo", true);
+        }});
+
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        closeWebDriver();
     }
 
 }
